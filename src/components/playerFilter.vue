bc4b3609e8526aca2b36da39fc1d513d0404f1c4
KeyboardDabbler <53971316+KeyboardDabbler@users.noreply.github.com>
2022-03-04 12:23:46 +1300

    Feat: playerFilters

---
+++ src/components/playerFilter.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex justify-between items-center text-sm">
+        <div class="flex">
+            <button
+                class="py-1 px-2 rounded-full bg-transparent border border-white text-white"
+            >First Team</button>
+            <button
+                class="py-1 px-2 mx-5 rounded-full bg-transparent border border-white text-white"
+            >Youth Team</button>
+            <button
+                class="py-1 px-2 rounded-full bg-transparent border border-white text-white"
+            >Manager</button>
+        </div>
+        <div class="grid">
+            <div class="mb-3">
+                <button
+                    class="py-1 px-2 rounded-full bg-transparent border border-white text-white"
+                >All Team</button>
+                <button
+                    class="py-1 px-2 mx-5 rounded-full bg-transparent border border-white text-white"
+                >Goalkeepers</button>
+                <button
+                    class="py-1 px-2 rounded-full bg-transparent border border-white text-white"
+                >Defenders</button>
+            </div>
+            <div class>
+                <button
+                    class="py-1 px-2 mr-5 rounded-full bg-transparent border border-white text-white"
+                >Midfielders</button>
+                <button
+                    class="py-1 px-2 rounded-full bg-transparent border border-white text-white"
+                >Forwards</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>

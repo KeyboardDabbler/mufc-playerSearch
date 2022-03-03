2c8157d2e432c63b18c4483bb69d38456be0a688
KeyboardDabbler <53971316+KeyboardDabbler@users.noreply.github.com>
2022-03-04 12:23:15 +1300

    Feat: btmNav

---
+++ src/components/btmNav.vue
@@ -0,0 +1,305 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="p-10">
+        <ul class="grid grid-cols-4 mb-10">
+            <li class="flex items-center justify-center">
+                <a href="/en/Partners/Global/adidas">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734544/Adidas_Comp_LightBG_180xAuto1547460175451.png"
+                        alt="Adidas"
+                        class="h-10"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/TeamViewer">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/56/997597/TeamViewer-logo-blue-footer1625085543829.png"
+                        alt="TeamViewer"
+                        class="h-10"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://tezos.com/manutd/">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/16/73/1067273/TezosLogo_Horizontal_Black_21644483053582.png"
+                        alt="Tezos"
+                        class="h-10"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Kohler">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734574/Kohler_Comp_LightBG_180xAuto1547470544622.png"
+                        alt="Kohler"
+                        class="h-10"
+                    />
+                </a>
+            </li>
+        </ul>
+
+        <ul class="grid grid-cols-10 gap-8 mb-10">
+            <li class="flex items-center justify-center">
+                <a href="/en/Partners/Global/Apollo-Tyres">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734550/Apollo_Comp_LightBG_180xAuto1547460405742.png"
+                        alt="Apollo Tyres"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.manutd.com/en/partners/global/cadbury">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/14/137/952608/Cadbury-new-2021-footer-log1609838831293.png"
+                        alt="Cadbury"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.manutd.com/en/Partners/Global/Canon-Medical-Systems">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/13/209/905560/Canon-Medical-2000x11251588088643865.jpg"
+                        alt="Canon Medical"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Casillero-del-Diablo">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734552/CDD_Comp_LightBG_180xAuto1547461184438.png"
+                        alt="Casillero del Diablo"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Chevrolet" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734554/Chevrolet_Comp_LightBG_180xAuto1547460485843.png"
+                        alt="Chevrolet"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Chivas" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734556/Chivas_Comp_LightBG_180xAuto1547460586288.png"
+                        alt="Chivas"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/DHL" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734562/DHL_Comp_LightBG_180xAuto1547461021282.png"
+                        alt="DHL"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Ecolab" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/101/1009129/Ecolab_Footer_Logo1628602152956.png"
+                        alt="Ecolab"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.hcltech.com/unitedbyhcl" target="_blank">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734570/HCL_Comp_LightBG_180xAuto1547462065520.png"
+                        alt="HCL"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Hua-Ti-Hui" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/32/991273/HTH_footer_logo1620932420633_img1x.png"
+                        alt="Hua Ti Hui"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+        </ul>
+
+        <ul class="grid grid-cols-9 gap-8 mt-5">
+            <li class="flex items-center justify-center">
+                <a href="/en/Partners/Global/Konami" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/12/52/799813/Konami_PartnerFooter1562073557221.png"
+                        alt="Konami"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.manutd.com/en/partners/global/marriott-hotels" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/12/194/836310/Marriott-onLightBG1571253423487.png"
+                        alt="Marriott Hotels"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Maui-Jim" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/198/771735/MauiJim-on-White-BG1550230930070.png"
+                        alt="Maui Jim"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Melitta" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734576/Melitta_Comp_LightBG_180xAuto1547470651869.png"
+                        alt="Melitta"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Mlily" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/204/773370/LOGO180x56B1550675933086.png"
+                        alt="Mlily"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.manutd.com/en/Partners/Global/remington" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/180/767217/Remington-Logo-OnWhiteBG1549538227044.png"
+                        alt="Remington"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Renewable-Energy-Group">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/73/1001760/RenewableEnergyGroup_Logo_PNG1626936371200.png"
+                        alt="Renewable Energy Group"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="/en/Partners/Global/Tag-Heuer" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/11/53/734586/TagHeuer_Comp_LightBG_180xAuto1547471156047.png"
+                        alt="Tag Heuer"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.manutd.com/en/Partners/Global/Visit-Malta" target="_self">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/12/95/810975/VisitMalta_WhiteBG1568380587328.png"
+                        alt="Visit Malta"
+                        class="h-8"
+                    />
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="socials p-5">
+        <ul class="soocials flex justify-center">
+            <li class="mx-4 flex justify-center items-center">
+                <a href="https://www.youtube.com/manutd">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/16/28/1055984/youtube-icon-40x401639670271302.png"
+                        alt="Follow us on YouTube External Website"
+                    />
+                </a>
+            </li>
+            <li class="mx-4 flex justify-center items-center">
+                <a href="https://www.facebook.com/manchesterunited ">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/55/997196/fb_2x1624877204997_img1x.png"
+                        alt="Follow us on Facebook External Website"
+                    />
+                </a>
+            </li>
+            <li class="mx-4 flex justify-center items-center">
+                <a href="https://twitter.com/ManUtd">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/55/997194/twitter_white_2x1624877758813_img1x.png"
+                        alt="Follow us on Via Twitter External Website"
+                    />
+                </a>
+            </li>
+            <li class="mx-4 flex justify-center items-center">
+                <a href="http://instagram.com/manchesterunited ">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/55/997197/instagram_2x1624877438061_img1x.png"
+                        alt="Follow us on Instagram External Website"
+                    />
+                </a>
+            </li>
+            <li class="mx-4 flex justify-center items-center">
+                <a href="https://www.tiktok.com/@manutd">
+                    <img
+                        src="//assets.manutd.com/AssetPicker/images/0/0/15/55/997190/tiktok_line1624878954741_img1x.png"
+                        alt="Follow us on Tik-Tok External Website"
+                    />
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="branding text-sm p-3 text-center">
+        <h6>
+            <a href="#">Privacy Policy</a> |
+            <a href="#">Terms &amp; Conditions</a>
+            <br />© 2020, All rights Reserved.
+            <br />Made with ❤️ by
+            <a href="#">KeyboardDabbler</a>
+        </h6>
+    </div>
+</template>
+
+<style scoped>
+.branding {
+    background-color: #1a1a1a;
+    border-top: 1px solid rgba(217, 217, 217, 0.3);
+    color: #fff;
+}
+
+.branding a {
+    color: #f7d144;
+}
+
+.socials {
+    background-color: #2a2a2a;
+}
+
+.soocials li {
+    width: 44px;
+    height: 44px;
+    background-color: #3f3f3f;
+    border-radius: 50%;
+}
+
+.soocials img {
+    width: 20px;
+    height: 20px;
+}
+</style>

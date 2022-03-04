eea3d04bfee12cd04823c228e1d9b1e5c4649c24
KeyboardDabbler <53971316+KeyboardDabbler@users.noreply.github.com>
2022-03-04 13:43:31 +1300

    Feat: player section

--- src/components/players.vue
+++ src/components/players.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 </script>
 
-<template></template>
+<template>
+    <h1 class="relative uppercase text-9xl text-center">
+        Manchester
+        <br />United
+    </h1>
+
+    <h3 class="absolute">sdgfsdrgsdgds</h3>
+</template>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap");
+h1 {
+    font-family: "Archivo Black", sans-serif;
+    color: #a1333c;
+}
 </style>
